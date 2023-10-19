8398e4e3e20b0c26d37f8d85e58591b362377d32
Jordan Lucy <99256155+JordanLucy@users.noreply.github.com>
2023-10-19 08:42:41 +0100

    Bank Holiday Testing

--- php/bankHolidays.php
+++ php/bankHolidays.php
@@ -14,15 +14,26 @@ curl_setopt($curl, CURLOPT_URL, $url);
 
 $result = curl_exec($curl);
 
+if ($result === false) {
+    $error = curl_error($curl);
+    $output['status']['code'] = "500";
+    $output['status']['name'] = "error";
+    $output['status']['description'] = "cURL Error: " . $error;
+} elseif (curl_getinfo($curl, CURLINFO_HTTP_CODE) == 200) {
+    $countryModalInfo = json_decode($result, true);
+    $output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+    $output['data'] = $countryModalInfo;
+} else {
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $output['status']['code'] = $httpCode;
+    $output['status']['name'] = "error";
+    $output['status']['description'] = "API Error: HTTP Status $httpCode";
+}
+
 curl_close($curl);
-
-$countryModalInfo = json_decode($result, true);
-
-$output['status']['code'] = "200";
-$output['status']['name'] = "ok";
-$output['status']['description'] = "success";
-$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = $countryModalInfo;
 
 header('Content-Type: application/json; charset=UTF-8');
 header("Access-Control-Allow-Origin: *");
